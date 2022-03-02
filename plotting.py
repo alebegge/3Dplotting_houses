89fd9635353241f101cc2375d2513ce5a07061a6
Alexandre Le Begge <alexandre.lebegge@gmail.com>
2022-03-02 15:54:26 +0100

    address module + 1st try shapefile

--- plotting.py
+++ plotting.py
@@ -7,27 +7,46 @@ from rasterio.plot import show
 import matplotlib.pyplot as plt
 import numpy as np
 import plotly.graph_objects as go
-
+import plotly.io as pio
+from geopy.geocoders import Nominatim
+import fiona
+import shapely.geometry
+import geopandas as gpd
 
 class Plotting():
     """
     This class object will regroup all technical functions to 
     plot a 3D houses based on our resources. 
     """
-    def __init__(self, x, y) -> None:
+    def __init__(self) -> None:
         """
         Have to enter x as lattitude and y as longitude.
         """
-        self.x = x
-        self.y = y
+        location = self.asking_address()
+        self.x = location.latitude
+        self.y = location.longitude
         self.left = 0
         self.bottom = 0
         self.right = 0
         self.top = 0
         self.dsm_gtif_path = os.path.join(os.path.abspath(''),Path("resources\DSM_nl_unzip\GeoTIFF"))
         self.dtm_gtif_path = os.path.join(os.path.abspath(''),Path("resources\DTM_nl_unzip\GeoTIFF"))
+    
+    def asking_address(self):
+        """
+        asking for an adress to the user and getting the gps coord from geopy.
+        should add a full loop + if statement for when adderess == None.
+        """
+        geolocator = Nominatim(user_agent='3Dplotting')
+        address_input = input("Enter an adress in Flanders or Brussels: ")
+        print(geolocator.geocode(address_input))
+        return geolocator.geocode(address_input)
+        # return self.location.latitude, self.location.longitude
 
     def transform_coord(self) -> float:
+        """
+        transform the coord from gps to lambert
+        """
         google_epsg = "epsg:4326"
         our_espg = "epsg:31370"
         ratio = Transformer.from_crs(google_epsg, our_espg)
@@ -46,7 +65,6 @@ class Plotting():
         self.top = (y + slice)
         self.bottom = (y - slice)
 
-
     def wich_file(self) -> str:
         """
         Regarding the bounds of each geotiff file,
@@ -56,7 +74,7 @@ class Plotting():
             gtif_path = os.path.join(self.dsm_gtif_path, gtif)
             tiff = rasterio.open(gtif_path)
             if (tiff.bounds.left < self.coord[0] <tiff.bounds.right) and (tiff.bounds.bottom < self.coord[1]< tiff.bounds.top):
-                print(f"Our coordinates are inside the {gtif} file.")
+                # print(f"Our coordinates are inside the {gtif} file.")
                 self.file_ok = gtif 
                 return gtif_path
     
@@ -83,14 +101,15 @@ class Plotting():
         tif_file_path = self.wich_file()
         with rasterio.open(tif_file_path) as src_dsm:
             window = rasterio.windows.from_bounds(self.left, self.bottom, self.right, self.top, transform=src_dsm.transform)
+            # window = rasterio.windows.from_bounds(self.shapefile_bounds())
+            # window = rasterio.windows.bounds(self.shapefile_bounds(), transform=src_dsm.transform)
             fd_dsm = src_dsm.read(1, window=window)
         with rasterio.open(self.dtm_path()) as src_dtm:
             fd_dtm = src_dtm.read(1, window=window)
         cols, row = np.meshgrid(np.arange(fd_dsm.shape[1]), np.arange(fd_dsm.shape[0]))
         fig = go.Figure(data=[go.Surface(y=cols, x=row, z=(fd_dsm+fd_dtm))])
-        # fig = go.Figure(data=[go.Surface(z=w)])
+        pio.renderers.default = 'browser'
         fig.show()
-
 
     def ref_file(self) -> str:
         """
@@ -99,20 +118,39 @@ class Plotting():
         file_inter = self.file_ok.replace("DHMVIIDSMRAS1m_", "")
         return file_inter[:-4]
 
+    def shapefile_path(self) ->str:
+        ref = self.ref_file()
+        if ref == "k05":
+            ref = "k5"
+        unzip_path = os.path.join(os.path.abspath(''),Path("resources\DSM_nl_unzip"))
+        return os.path.join(unzip_path, f"DHMVII_vdc_{ref}.shp")
+
     def dtm_path(self):
         # self.wich_file()
         dtm_file_name = self.file_ok.replace("DHMVIIDSMRAS1m_", "DHMVIIDTMRAS1m_")
         dtm_path = os.path.join(self.dtm_gtif_path, dtm_file_name)
         return dtm_path
+    
+    # def shapefile_bounds(self):
+    #     """
+    #     Taking the polygon value of our adderess coord. 
+    #     """
+    #     shape = gpd.read_file(self.shapefile_path())
+    #     shape_bounds = shape.cx[self.left:self.right, self.bottom:self.top]
+    #     polygon = shapely.geometry.Polygon(shape_bounds["geometry"])
+    #     return polygon.bounds
+
+        
 
 
 
-addres = Plotting(50.84722123483747, 4.485519972234445)
+addres = Plotting()
 addres.transform_coord()
 addres.wich_file()
-# # print(addres.x_bounds())
-# # print(addres.y_bounds())
-addres.plot_3d(slice=25)
+# # # print(addres.x_bounds())
+# # # print(addres.y_bounds())
+print(addres.shapefile_path())
+addres.plot_3d(slice=100)
 
 # # # # print(addres.ref_file())
 # # # addres.plot_2d(slice=250)
--- test.py
+++ test.py
@@ -5,19 +5,60 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 import rioxarray
 
-test_wo = Plotting(50.83330365407295, 4.486695401854129)
-test_wo.transform_coord()
+# test_wo = Plotting(50.83330365407295, 4.486695401854129)
+# test_wo.transform_coord()
 
-path_gtif = test_wo.wich_file()
+# path_gtif = test_wo.wich_file()
 
-# da = xr.open_rasterio(path_gtif)
-da = rioxarray.open_rasterio(path_gtif)
-transform = Affine.from_gdal(*da.attrs["transform"])
+# # da = xr.open_rasterio(path_gtif)
+# da = rioxarray.open_rasterio(path_gtif)
+# transform = Affine.from_gdal(*da.attrs["transform"])
 
-fig = plt.figure(figsize=(15,15))
-ax = fig.add_subplot(111)
-ax.imshow(da.variable.data[0])
-ax.set_xlim(test_wo.x_bounds_calc(slice=250))
-ax.set_ylabel(test_wo.y_bounds_calc(slice=250))
-plt.show()
+# fig = plt.figure(figsize=(15,15))
+# ax = fig.add_subplot(111)
+# ax.imshow(da.variable.data[0])
+# ax.set_xlim(test_wo.x_bounds_calc(slice=250))
+# ax.set_ylabel(test_wo.y_bounds_calc(slice=250))
+# plt.show()
 
+import fiona
+import rasterio
+import shapely.geometry
+import numpy as np
+
+from rasterstats.io import Raster
+from PIL import Image
+
+test=Plotting()
+test.transform_coord()
+
+
+tif_filename = test.wich_file()
+dtm_file = test.dtm_path()
+shape_file = test.shapefile_path()
+with Raster(tif_filename, band=1) as dsm_file:
+    with Raster(dtm_file, band=1) as dtm_file:
+        index = 0
+        for feat in fiona.open(shape_file):
+            polygon_geometry = feat['geometry']
+            polygon = shapely.geometry.Polygon(polygon_geometry['coordinates'][0])
+            polygon_bounds = polygon.bounds
+
+            raster_subset_1 = dsm_file.read(bounds=polygon_bounds)
+            polygon_mask = rasterio.features.geometry_mask(geometries=[polygon_geometry],
+                                                out_shape=(raster_subset_1.shape[0],raster_subset_1.shape[1]),
+                                                transform=raster_subset_1.affine,
+                                                all_touched=False,
+                                                invert=True)
+
+            raster_subset_2 = dtm_file.read(bounds=polygon_bounds)
+
+
+            masked_1 = raster_subset_1.array * polygon_mask
+            masked_2 = raster_subset_2.array * polygon_mask
+
+            masked_all = np.dstack([masked_1, masked_2])
+
+            img = Image.fromarray(masked_all[:, :, :].astype('uint8'), 'RGB')
+            img.save('out/' + str(index) + '.jpg')
+            index += 1
